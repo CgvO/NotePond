3a1dcb65f7432e73ae1f6e24440434ad1485bf6e
CgvO <cgv@uoregon.edu>
2023-05-21 14:55:58 -0700

    pushed an initial version of search_files (no clue if its correct or even works)

---
+++ NotePond/common/filter.py
@@ -0,0 +1,8 @@
+from django.db.models import Q
+from models import Note
+
+def search_files(search_results):
+    files = Note.objects.filter(
+        Q(title__icontains=search_results) | Q(tags__name__icontains=search_results)
+    )
+    return files
