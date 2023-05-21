d384a3185ed04f8b19c4354822f5f4d8e5c047c3
unknown <dch@uoregon.edu>
2023-05-21 13:10:20 -0700

    basic views

--- NotePond/common/views.py
+++ NotePond/common/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def noteSearch(request):
+    return render(request, 'base.html')
+
+def noteView(request):
+    return render(request, 'base.html')
+
+def noteUpload(request):
+    return render(request, 'base.html')
