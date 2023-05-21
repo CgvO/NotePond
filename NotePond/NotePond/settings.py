9b967a7e9eb7b85b84d8cfb6f2f81d376755f770
zacharyweisenbloom <38918997+zacharyweisenbloom@users.noreply.github.com>
2023-05-21 14:11:36 -0700

    Added installed apps in settings

--- NotePond/NotePond/settings.py
+++ NotePond/NotePond/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "common.apps.SearchConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
