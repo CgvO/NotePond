6c47d38daa47eb1bfd23afd1423b6f1e303fedc5
CgvO <cgv@uoregon.edu>
2023-06-08 22:53:29 -0700

    heroku

--- NotePond/NotePond/settings.py
+++ NotePond/NotePond/settings.py
@@ -28,15 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#fc0klydve2c(7&16i5r94(&h&9^0tnd3+6sz=k@klv9%xd=hm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
+'''
 DEBUG = True
 
 ALLOWED_HOSTS = []
 '''
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','notepond.herokuapp.com/']
-'''
+ALLOWED_HOSTS = ['notepond.herokuapp.com/']
+
 
 # Application definition
 
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_select2",
 ]
-
+'''
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -72,7 +72,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-'''
+
 ROOT_URLCONF = "NotePond.urls"
 
 TEMPLATES = [
@@ -149,8 +149,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'notes/')
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 #XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
-
+'''
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 django_heroku.settings(locals())
+'''
--- NotePond/NotePond/urls.py
+++ NotePond/NotePond/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from common import views
-
+'''
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="index"),
@@ -35,6 +35,7 @@ urlpatterns = [
 ]
 '''
 from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -49,9 +50,9 @@ urlpatterns = [
     path('vote/<int:note_id>/<str:vote_type>/', views.vote, name='vote'),
     path('select2/', include("django_select2.urls")),
 
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
-'''
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
