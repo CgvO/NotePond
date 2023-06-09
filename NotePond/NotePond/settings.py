62e6a0c16f7b4d82597a697998d9d9c5fd288ece
CgvO <cgv@uoregon.edu>
2023-06-09 14:25:31 -0700

    heroku

--- NotePond/NotePond/settings.py
+++ NotePond/NotePond/settings.py
@@ -28,11 +28,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#fc0klydve2c(7&16i5r94(&h&9^0tnd3+6sz=k@klv9%xd=hm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
+'''
 DEBUG = True
 
 ALLOWED_HOSTS = []
+'''
+DEBUG = False
 
+ALLOWED_HOSTS = ['notepond.herokuapps.com/']
 
 # Application definition
 
@@ -46,7 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_select2",
 ]
-
+'''
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -55,6 +58,17 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+'''
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 
