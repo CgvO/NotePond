857d6f87832f8abad65b02fd6c295e37bf6b29c4
CgvO <cgv@uoregon.edu>
2023-06-08 18:24:42 -0700

    heroku please

--- NotePond/NotePond/settings.py
+++ NotePond/NotePond/settings.py
@@ -28,15 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#fc0klydve2c(7&16i5r94(&h&9^0tnd3+6sz=k@klv9%xd=hm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
+'''
+DEBUG = True
 
-DEBUG = False
-
-ALLOWED_HOSTS = ["notepond.herokuapp.com"]
+ALLOWED_HOSTS = []
 '''
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','notepond.herokuapp.com/']
-'''
+
 
 # Application definition
 
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     "django_select2",
 ]
 
-#Middleware below for heroku
+'''
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+#heroku middleware
 '''
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -72,7 +73,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-'''
 
 ROOT_URLCONF = "NotePond.urls"
 
