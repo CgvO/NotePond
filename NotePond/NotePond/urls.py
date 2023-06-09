bf0e72879bb035eacd994ff8aff77138bd9035a3
CgvO <cgv@uoregon.edu>
2023-06-08 22:54:38 -0700

    heroku

--- NotePond/NotePond/urls.py
+++ NotePond/NotePond/urls.py
@@ -50,8 +50,8 @@ urlpatterns = [
     path('vote/<int:note_id>/<str:vote_type>/', views.vote, name='vote'),
     path('select2/', include("django_select2.urls")),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
