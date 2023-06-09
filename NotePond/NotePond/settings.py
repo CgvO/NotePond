6bc64b42fae3b77434dcdce024b73bd160091d96
CgvO <cgv@uoregon.edu>
2023-06-09 14:01:00 -0700

    heroku

--- NotePond/NotePond/settings.py
+++ NotePond/NotePond/settings.py
@@ -151,6 +151,6 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 #XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Static'), )
 
 django_heroku.settings(locals())
